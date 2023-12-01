bd3b714e9116eb014270d812e4761ca92f9d4f46
Mohammad Joynul Abedin <shokal.ash21016@gmail.com>
2023-12-01 16:52:25 +0600

    4th Push

--- Models/NewArticles.js
+++ Models/NewArticles.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://shokalash21016:Cljia1I7pLgFIBgv@has01.veerjhz.mongodb.net/?retryWrites=true&w=majority', { // Replace with your MongoDB connection string
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+const NewsArticleSchema = new mongoose.Schema({
+    title: String,
+    link: String,
+    image: String,
+    source: String,
+    category: String,
+    dateFetched: { type: Date, default: Date.now }
+});
 
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }
-};
+module.exports = mongoose.model('NewsArticle', NewsArticleSchema);
 
-module.exports = connectDB;
--- app.js
+++ app.js
@@ -1,11 +1,96 @@
+// const express = require('express');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+// const app = express();
+
+
+// // Function to fetch news from a given URL and category
+// const fetchNews = async (categoryPath, category) => {
+//     const baseUrl = 'https://edition.cnn.com';
+//     let articles = [];
+//     let id = 0;
+
+//     try {
+//         const fullUrl = baseUrl + categoryPath;
+//         const response = await axios.get(fullUrl);
+//         const $ = cheerio.load(response.data);
+//         $('div.card').each((i, element) => {
+//             const aTag = $(element).find('a');
+//             const headlineTag = $(element).find('span[data-editable="headline"]');
+//             const imageTag = $(element).find('img');
+
+//             if (aTag && headlineTag && imageTag) {
+//                 let link = aTag.attr('href');
+//                 // Ensure the link is absolute
+//                 if (link.startsWith('/')) {
+//                     link = baseUrl + link;
+//                 }
+
+//                 const title = headlineTag.text();
+//                 const imageLink = imageTag.attr('src');
+//                 const source = "CNN";
+//                 news_id = id+1,
+//                 articles.push({
+//                     id: news_id,
+//                     title: title ?? '',
+//                     link: link ?? '',
+//                     image: imageLink ?? '',
+//                     source: source?? '',
+//                     category: category
+//                 });
+//                 id = news_id
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Error fetching news:', error);
+//     }
+
+//     return articles;
+// };
+
+// // Endpoint for fetching all news
+// app.get('/', async (req, res) => {
+//     const categories = [
+//         { path: '/', category: 'All' },
+//         { path: '/world', category: 'World' },
+//         { path: '/entertainment', category: 'Entertainment' },
+//         { path: '/health', category: 'Health' },
+//         { path: '/business/tech', category: 'Technology' },
+//         { path: '/sport', category: 'Sports' },
+//         { path: '/politics', category: 'Politics' },
+//         { path: '/business', category: 'Business' }
+//         // Add more categories if needed
+//     ];
+
+//     let allArticles = [];
+//     for (const category of categories) {
+//         const articles = await fetchNews(category.path, category.category);
+//         allArticles = allArticles.concat(articles);
+//     }
+
+//     res.json(allArticles);
+// });
+
+
+// // Start the server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-
+const connectDB = require('./DataBase/DB'); // Import the database connection
+const NewsArticle = require('./Models/NewArticles'); // Import the model
 const app = express();
 
+// Connect to MongoDB
+connectDB();
 
-// Function to fetch news from a given URL and category
+// fetchNews function
 const fetchNews = async (categoryPath, category) => {
     const baseUrl = 'https://edition.cnn.com';
     let articles = [];
@@ -15,6 +100,7 @@ const fetchNews = async (categoryPath, category) => {
         const fullUrl = baseUrl + categoryPath;
         const response = await axios.get(fullUrl);
         const $ = cheerio.load(response.data);
+
         $('div.card').each((i, element) => {
             const aTag = $(element).find('a');
             const headlineTag = $(element).find('span[data-editable="headline"]');
@@ -30,16 +116,16 @@ const fetchNews = async (categoryPath, category) => {
                 const title = headlineTag.text();
                 const imageLink = imageTag.attr('src');
                 const source = "CNN";
-                news_id = id+1,
+                id++;
+
                 articles.push({
-                    id: news_id,
-                    title: title ?? '',
-                    link: link ?? '',
-                    image: imageLink ?? '',
-                    source: source?? '',
+                    id: id,
+                    title: title,
+                    link: link,
+                    image: imageLink,
+                    source: source,
                     category: category
                 });
-                id = news_id
             }
         });
     } catch (error) {
@@ -49,29 +135,49 @@ const fetchNews = async (categoryPath, category) => {
     return articles;
 };
 
+// Define categories
+const categories = [
+    { path: '/', category: 'All' },
+    { path: '/world', category: 'World' },
+    { path: '/entertainment', category: 'Entertainment' },
+    { path: '/health', category: 'Health' },
+    { path: '/business/tech', category: 'Technology' },
+    { path: '/sport', category: 'Sports' },
+    { path: '/politics', category: 'Politics' },
+    { path: '/business', category: 'Business' }
+    // Add more categories if needed
+];
+
 // Endpoint for fetching all news
 app.get('/', async (req, res) => {
-    const categories = [
-        { path: '/', category: 'All' },
-        { path: '/world', category: 'World' },
-        { path: '/entertainment', category: 'Entertainment' },
-        { path: '/health', category: 'Health' },
-        { path: '/business/tech', category: 'Technology' },
-        { path: '/sport', category: 'Sports' },
-        { path: '/politics', category: 'Politics' },
-        { path: '/business', category: 'Business' }
-        // Add more categories if needed
-    ];
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        let existingNews = await NewsArticle.find({ dateFetched: { $gte: today } });
 
-    let allArticles = [];
-    for (const category of categories) {
-        const articles = await fetchNews(category.path, category.category);
-        allArticles = allArticles.concat(articles);
+        if (existingNews.length > 0) {
+            res.json(existingNews);
+        } else {
+            let allArticles = [];
+            
+            for (const category of categories) {
+                const articles = await fetchNews(category.path, category.category);
+                allArticles = allArticles.concat(articles);
+            }
+
+            // Save new articles to MongoDB
+            await NewsArticle.insertMany(allArticles.map(article => ({
+                ...article,
+                dateFetched: new Date() // Add the dateFetched field
+            })));
+
+            res.json(allArticles);
+        }
+    } catch (error) {
+        console.error('Error: ', error);
+        res.status(500).send('Server error');
     }
-
-    res.json(allArticles);
 });
-
 
 // Start the server
 const PORT = process.env.PORT || 3000;
